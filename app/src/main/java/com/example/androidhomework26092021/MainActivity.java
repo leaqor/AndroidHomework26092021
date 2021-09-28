b8179849823e4c5dca5f2630365c0fab83fc0eab
leaqor <thomasmann334@gmail.com>
2021-09-28 21:22:39 +0300

    Ready for homework review , sorry for initial message.

--- app/src/main/java/com/example/androidhomework26092021/MainActivity.java
+++ app/src/main/java/com/example/androidhomework26092021/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidhomework26092021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button evaluateBtn = findViewById(R.id.evaluate_btn);
+
+        evaluateBtn.setOnClickListener (v -> {
+            setContentView(R.layout.activity_main2);
+    });
     }
+
 }
